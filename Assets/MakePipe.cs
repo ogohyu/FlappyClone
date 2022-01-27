7df985783dac0b3085bf6e1875029b7babf8e51b
yoyhyu <66709690+yoyhyu@users.noreply.github.com>
2022-01-27 17:18:36 +0900

    플래피버드 클론코딩 (유튜브 조코딩)
    유니티 입문

---
+++ Assets/MakePipe.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakePipe : MonoBehaviour
+{
+    public GameObject pipe;
+    public float timeDiff;
+    float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > timeDiff)
+        {
+            GameObject newPipe = Instantiate(pipe);
+            newPipe.transform.position = new Vector3(5, Random.Range(-2.65f, 4.15f), 0);
+            timer = 0;
+            Destroy(newPipe, 10.0f);
+        }
+    }
+}
